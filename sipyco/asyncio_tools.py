b10d70daf815d9af99ac6af9ded26a644bb1dfd3
David Nadlinger <code@klickverbot.at>
2020-12-01 23:18:37 +0100

    AsyncioServer: Fix check for unhandled exceptions vs. cancellations
    As per the asyncio documentation, trying to access Task.exception()
    *raises* CancelledError when the task has been cancelled, but the
    code had been written with the assumption that it *returns* one.

--- sipyco/asyncio_tools.py
+++ sipyco/asyncio_tools.py
@@ -74,7 +74,7 @@ class AsyncioServer:
 
     def _client_done(self, task):
         self._client_tasks.remove(task)
-        if task.exception() and task.exception() != asyncio.CancelledError:
+        if not task.cancelled() and task.exception():
             logger.error("Client connection closed with error", exc_info=True)
 
     def _handle_connection(self, reader, writer):
