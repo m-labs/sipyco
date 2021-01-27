a9bdc9c636fc87fea8483e2ba4e6a26a7b650957
Sebastien Bourdeauducq <sb@m-labs.hk>
2021-01-27 13:07:48 +0800

    setup.py: bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sipyco",
-    version="1.1",
+    version="1.2",
     author="M-Labs",
     url="https://m-labs.hk/artiq",
     description="Simple Python communications",
