0d61fba000d03692ee5aa66c813977e7db085c31
Sebastien Bourdeauducq <sb@m-labs.hk>
2022-04-05 10:03:00 +0800

    setup.py: stop checking for dependencies
    setuptools sucks and the dependency check causes more problems than it solves.

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
 
+# Depends on numpy and optionally pybase64
+
 setup(
     name="sipyco",
     version="1.2",
@@ -7,7 +9,6 @@ setup(
     url="https://m-labs.hk/artiq",
     description="Simple Python communications",
     license="LGPLv3+",
-    install_requires=["setuptools", "numpy", "pybase64"],
     packages=find_packages(),
     entry_points={
         "console_scripts": [
