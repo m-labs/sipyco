041850406e66d698b4fd1e8d275e0691092b572a
Sebastien Bourdeauducq <sb@m-labs.hk>
2025-05-26 09:32:59 +0800

    NixOS 25.05

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Simple Python Communications";
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-24.11;
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-25.05;
 
   outputs = { self, nixpkgs }:
     let
